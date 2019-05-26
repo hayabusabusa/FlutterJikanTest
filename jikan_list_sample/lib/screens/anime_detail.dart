6bd791ca0150dcef54980aa4bef440384ac834ea
hayabusabusa <31949692+hayabusabusa@users.noreply.github.com>
2019-05-27 00:28:42 +0900

    Add detail screen

---
+++ jikan_list_sample/lib/screens/anime_detail.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+import 'package:jikan_list_sample/model/anime.dart';
+
+class AnimeDetail extends StatelessWidget {
+  Anime anime;
+
+  AnimeDetail(
+    this.anime
+  );
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
