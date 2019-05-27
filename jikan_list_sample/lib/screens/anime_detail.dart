9a9039d3aaf97e143479f3a810a28bb52f50a14a
hayabusabusa <31949692+hayabusabusa@users.noreply.github.com>
2019-05-28 00:08:01 +0900

    Fix detail screen layout

--- jikan_list_sample/lib/screens/anime_detail.dart
+++ jikan_list_sample/lib/screens/anime_detail.dart
@@ -8,6 +8,22 @@ class AnimeDetail extends StatelessWidget {
     this.anime
   );
 
+  Widget _buildContentWidget(String title, String content) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 16.0, vertical: 8.0),
+      child: Container(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: <Widget>[
+            Text(title),
+            Divider(color: Colors.grey[350],),
+            Text(content),
+          ],
+        ),
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -22,6 +38,18 @@ class AnimeDetail extends StatelessWidget {
                 fit: BoxFit.cover,
               ),
             ),
+          ),
+          SliverList(
+            delegate: SliverChildListDelegate([
+              Column(
+                children: <Widget>[
+                  SizedBox(height: 16.0),
+                  _buildContentWidget("Anime title", anime.title),
+                  _buildContentWidget("Anime description", "This widget shows anime description."),
+                  _buildContentWidget("Anime tag", "Tag is here ?")
+                ],
+              ),
+            ]),
           ),
         ],
       ),
